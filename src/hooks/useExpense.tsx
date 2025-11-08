3f83e24ded6a6802385b0f2f246d22fbe5334d1b
Lucas Bailo <lucasebailo@gmail.com>
2025-11-08 19:33:23 -0300

    fix: fixing get

--- src/hooks/useExpense.tsx
+++ src/hooks/useExpense.tsx
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import axios from 'axios';
 
 interface ExpenseRequest {
-  amount: number;
-  date: string; // formato ISO: ex. "2025-11-08T17:32:28Z"
-  description: string;
-  id?: string;
-  type: string;
+    amount: number;
+    date: string; // formato ISO: ex. "2025-11-08T17:32:28Z"
+    description: string;
+    id?: string;
+    type: string;
 }
 
 interface ExpenseResponse {
-  id: string;
-  amount: number;
-  date: string;
-  description: string;
-  type: string;
+    id: string;
+    amount: number;
+    date: string;
+    description: string;
+    type: string;
 }
 
 const useExpense = () => {
@@ -22,22 +22,25 @@ const useExpense = () => {
     const [error, setError] = useState<string | null>(null);
     const [expenses, setExpenses] = useState<ExpenseResponse | null>(null);
 
-    const getExpense = async ( data: ExpenseRequest ) => {
+    const getExpense = async (id?: string) => {
         setLoading(true);
         setError(null);
         try {
-            const response = await axios.get<ExpenseResponse>(
-                'https://localhost:7116/api/Expense',
-            );
+            const url = id
+                ? `https://localhost:7116/api/Expense/${id}`
+                : `https://localhost:7116/api/Expense`;
+
+            const response = await axios.get<ExpenseResponse>(url);
             setExpenses(response.data);
-        } catch (err: any) {
-            setError('Erro ao cadastrar despesa');
+        } catch (err) {
+            console.error(err);
+            setError('Erro ao buscar despesa');
         } finally {
             setLoading(false);
         }
     };
 
-    const postExpense = async ( data: ExpenseRequest ) => {
+    const postExpense = async (data: ExpenseRequest) => {
         setLoading(true);
         setError(null);
         try {
@@ -47,6 +50,21 @@ const useExpense = () => {
             );
         } catch (err: any) {
             setError('Erro ao cadastrar despesa');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const putExpense = async (data: ExpenseRequest) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.put<ExpenseResponse>(
+                'https://localhost:7116/api/Expense/' + data.id,
+                data
+            );
+        } catch (err: any) {
+            setError('Erro ao editar despesa');
         } finally {
             setLoading(false);
         }
