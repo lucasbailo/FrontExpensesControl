f9a74e3fc9314bbe8c98d14a06afad1527335dfa
Lucas Bailo <lucasebailo@gmail.com>
2025-11-08 19:30:42 -0300

    create: creating getExpense

--- src/hooks/useExpense.tsx
+++ src/hooks/useExpense.tsx
@@ -22,13 +22,12 @@ const useExpense = () => {
     const [error, setError] = useState<string | null>(null);
     const [expenses, setExpenses] = useState<ExpenseResponse | null>(null);
 
-    const postExpense = async ( data: ExpenseRequest ) => {
+    const getExpense = async ( data: ExpenseRequest ) => {
         setLoading(true);
         setError(null);
         try {
-            const response = await axios.post<ExpenseResponse>(
+            const response = await axios.get<ExpenseResponse>(
                 'https://localhost:7116/api/Expense',
-                data
             );
             setExpenses(response.data);
         } catch (err: any) {
@@ -38,7 +37,22 @@ const useExpense = () => {
         }
     };
 
-    return { postExpense, loading, error, expenses };
+    const postExpense = async ( data: ExpenseRequest ) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await axios.post<ExpenseResponse>(
+                'https://localhost:7116/api/Expense',
+                data
+            );
+        } catch (err: any) {
+            setError('Erro ao cadastrar despesa');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { getExpense, postExpense, loading, error, expenses };
 };
 
 export default useExpense;
