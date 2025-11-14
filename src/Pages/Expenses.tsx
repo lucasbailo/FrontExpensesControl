64dd03710543dab0e307a36d8257da0bcb69c7d8
Lucas Bailo <lucasebailo@gmail.com>
2025-11-13 22:19:01 -0300

    style: adding delete button

--- src/Pages/Expenses.tsx
+++ src/Pages/Expenses.tsx
@@ -83,6 +83,15 @@ export default function Expenses() {
                   >
                     Editar
                   </button>
+                  <button
+                    className="text-red-600 hover:underline mr-3"
+                    onClick={() => {
+                      setSelected(item);
+                      setIsModalOpen(true);
+                    }}
+                  >
+                    Deletar
+                  </button>
                 </td>
               </tr>
             ))}
